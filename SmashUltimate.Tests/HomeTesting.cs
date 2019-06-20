77165da1e8fa9e88332ace59f800704f84231e4b
A113838 <A113838@ND447173>
2019-06-20 10:33:01 -0600

    Add project files.

---
+++ SmashUltimate.Tests/HomeTesting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmashUltimate.Controllers;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using SmashUltimate.Models;
+
+namespace SmashUltimate.Tests
+{
+    [TestClass]
+    public class HomeTesting : ApiController
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            HomeController homeController = new HomeController();
+            //Act
+            IHttpActionResult httpResult = homeController.DefaultCall();
+            //Assert
+            Assert.IsInstanceOfType(httpResult, typeof(OkNegotiatedContentResult<GameModel>));
+        }
+    }
+}
---
+++ SmashUltimate/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using SmashUltimate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmashUltimate.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route("home")]
+        public IHttpActionResult DefaultCall()
+        {
+            GameModel newGame = new GameModel();
+            newGame.gameGeneration = "Ultimate";
+            newGame.patchNumber = 3.0;
+            newGame.player1 = "mario";
+            newGame.player2 = "link";
+
+            return Ok(newGame);
+        }
+    }
+}
